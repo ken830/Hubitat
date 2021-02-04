0856fd447d3533bdbd0d7cb56ad92f7a5dace1a8
Kenneth Leung <230590+ken830@users.noreply.github.com>
2021-02-04 02:58:42 -0800

    Update hikvision-alarm.driver.groovy

--- drivers/Hikvision Alarm Driver/hikvision-alarm.driver.groovy
+++ drivers/Hikvision Alarm Driver/hikvision-alarm.driver.groovy
@@ -3,7 +3,7 @@
  * 
  * Copyright 2021 Kenneth Leung (ken830)
  * 
- * ** Setup **
+ * ** Hikvision Camera Setup **
  * 1. Configuration -> Network -> Advanced Settings -> HTTP Listening: DestinationIP = [HubitatIP], URL = "/", Port = 39501
  * 2. Enable "Notify Surveillance Center" under Linkage Method for each event
  *
@@ -22,26 +22,26 @@ metadata {
 		capability "MotionSensor"
 		capability "PresenceSensor"
 		
-        command 'resetAlerts'
+        command "resetAlerts"
 
     }
 }
 
 preferences {
-    input name: "hikIP", type: 'string', title:'<b>Camera IP Address</b>', description: '<div><i></i></div><br>', required: true
-    input name: "resetTime", type: "number", title:"<b>Alert Reset Time (sec)</b>", description: '<div><i></i></div><br>', defaultValue: 0
+    input name: "hikIP", type: "string", title:"<b>Camera IP Address</b>", description: "<div><i></i></div><br>", required: true
+    input name: "resetTime", type: "number", title:"<b>Alert Reset Time (sec)</b>", description: "<div><i></i></div><br>", defaultValue: 0
 	
-	input name: 'loggingEnabled', type: 'bool', title: '<b>Enable Logging</b>', description: '<div><i></i></div><br>', defaultValue: false
+	input name: "loggingEnabled", type: "bool", title: "<b>Enable Logging</b>", description: "<div><i></i></div><br>", defaultValue: false
 }
 
 def installed() {
-    log.debug 'Hikvision Alarm device installed'
+    log.debug "Hikvision Alarm device installed"
 	
 	configure()
 }
 
 def updated() {
-	log.debug 'Hikvision Alarm device updated'
+	log.debug "Hikvision Alarm device updated"
 
 	configure()
 }
@@ -49,7 +49,7 @@ def updated() {
 def configure() {
 	unschedule()
 	
-    state.remove('connectionStatus')
+    state.remove("connectionStatus")
 	
 	setNetworkAddress()
 
@@ -57,7 +57,7 @@ def configure() {
 	resetAlerts()
 	
 	// Run Watchdog check every 1 minute
-    runEvery1Minute('watchdog')
+    runEvery1Minute("watchdog")
 }
 
 def resetAlerts() {
@@ -65,7 +65,7 @@ def resetAlerts() {
 }
 
 def parse(String description) {
-    logDebug "Parsing '${description}'"
+    logDebug "Parsing "${description}""
 
 	def msg = parseLanMessage(description)   
     def body = new XmlSlurper().parseText(new String(msg.body))
@@ -105,18 +105,18 @@ void watchdog() {
         // check if there have been any messages in the last 1 minute
         if(state.lastReport >= now() - (1 * 60 * 1000)) {
             // OKAY
-            logDebug 'watchdog: OKAY'
+            logDebug "watchdog: OKAY"
         }
         else {
             // FAULT
-            log.warn 'watchdog: FAULT'
-            // if we don't receive any messages within 1 minute
+            log.warn "watchdog: FAULT"
+            // if we don"t receive any messages within 1 minute
             // set state to inactive
 			//sendEvent(name: "motion", value: "inactive")
         }
     }
     else {
-        log.info 'No previous reports. Cannot determine health.'
+        log.info "No previous reports. Cannot determine health."
     }
 }
 
@@ -125,7 +125,7 @@ private Integer convertHexToInt(hex) {
 }
 
 private String convertIPtoHex(ipAddress) { 
-    String hex = ipAddress.tokenize( '.' ).collect {  String.format( '%02x', it.toInteger() ) }.join()
+    String hex = ipAddress.tokenize( "." ).collect {  String.format( "%02x", it.toInteger() ) }.join()
     return hex.toUpperCase()
 }
 
@@ -134,7 +134,7 @@ private String utc2000ToDate(int seconds) {
     // <UNIX time> = <2000 time> + <January 1, 2000 UNIX time>
     int unixSeconds = seconds + unix200Time
     long unixMilliseconds = unixSeconds * 1000L
-    new Date(unixMilliseconds).format('yyyy-MM-dd h:mm', location.timeZone)
+    new Date(unixMilliseconds).format("yyyy-MM-dd h:mm", location.timeZone)
 }
 
 void logDebug(str) {
